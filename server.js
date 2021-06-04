03e42a174392d80d8afb7bbd757e5d5da4f3f649
comfybear71 <stuart@psal.net.au>
2021-06-04 18:52:39 +0930

    first instance of project 1

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+app = express();
+port = process.env.PORT || 5000;
+cors = require('cors');
+
+app.use(cors());
+app.listen(port, () => console.log('Server Listening on port: ' + port));
+
+app.get('/', (req, res) => {
+    res.send({message: 'We made it'})
+})
