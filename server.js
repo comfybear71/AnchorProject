6eec08fd313be5585eb0f9bbaa612332cae45d3a
comfybear71 <stuart@psal.net.au>
2021-06-04 23:00:16 +0930

    Update for project 2

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(cors());
 app.listen(port, () => console.log('Server Listening on port: ' + port));
 
 app.get('/', (req, res) => {
-    res.send({message: 'We are ready and waiting'})
+    res.send({message: 'We are moving on with Project 2'})
 })
